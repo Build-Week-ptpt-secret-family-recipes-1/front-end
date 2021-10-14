5e7e5b6dec240739bb350953ce6f0b7a2e04ccd7
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-10-13 22:34:53 -0400

    initial start of homepage

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './HomePage.css'
+
+export default function HomePage () {
+    return (
+        <div>
+            <h1>Secret Family Recipes</h1>
+            <p>Have a recipe? Share it!</p>
+        </div>
+    )
+}
