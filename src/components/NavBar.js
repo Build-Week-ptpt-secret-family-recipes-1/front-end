ffc8b31739d51301eabce00ead77aa912de98dab
quinn5moli <quinn.moli@gmail.com>
2021-10-22 15:06:16 -0700

    integrate sidebar and search by type to home; add and integrate navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
+import { BASE_URL } from "../utils";
+
+const Navigation = ({ history }) => {
+    const logOut = () => {
+        localStorage.removeItem('token');
+        history.push('/login');
+    };
+
+    return (
+        <div className="App">
+            <nav className="nav">
+        <div className="title-bar">
+          <h1>Secret Family Recipes</h1>
+        </div>
+        <div className="nav-links">
+          <a href={BASE_URL} target="_blank">Home</a>
+          <NavLink to="/">Home Page</NavLink>
+          <NavLink to="/add-recipe">Add New Recipe</NavLink>
+          <button onClick={logOut}>Sign Out</button>
+        </div>
+      </nav>
+        </div>
+    )
+}
+
+export default withRouter(Navigation);
