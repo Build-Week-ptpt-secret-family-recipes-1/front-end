e0c3759922d18e6285cd0c3f0c204f2958961c3b
quinn5moli <quinn.moli@gmail.com>
2021-10-23 18:06:30 -0700

    rendered login and register form on their corresponding components; rendered components on index.js

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LogInForm from "../components/forms/LoginForm";
+
+const Login = () => {
+    return (
+        <>
+            <LogInForm />
+        </>
+    )
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals'
 import thunk from 'redux-thunk'
-
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './utils/reducers'
+import HomePage from './components/HomePage';
+import AddRecipe from './components/recipe components/AddRecipe';
+import Registration from './components/Registration';
+import Login from './components/Login';
+import Recipe from './components/recipe components/Recipe';
+import EditRecipe from './components/recipe components/EditRecipe';
 
-const store = createStore(reducer, applyMiddleware(thunk));;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk))
+);
+
+//not sure if add recipe endpoint will work, added for display of logic
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <Switch>
+        <PrivateRoute exact path='/' component={HomePage} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Registration} />
+        <PrivateRoute path='/recipes/view/:id' component={Recipe} />
+        <PrivateRoute path='/recipes/edit/:id' component={EditRecipe} />
+        <PrivateRoute path='/add-recipe' component={AddRecipe} />
+        </Switch>
     </Router>
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
